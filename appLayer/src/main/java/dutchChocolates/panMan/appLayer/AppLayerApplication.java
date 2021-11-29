f35f9ed5675cfe5ef67172c85064ec2dc8a8d14d
haruncansurav <can.surav@ug.bilkent.edu.tr>
2021-11-29 16:45:18 +0300

    Application Layer for PanMan is initialized.

---
+++ appLayer/src/main/java/dutchChocolates/panMan/appLayer/AppLayerApplication.java
@@ -0,0 +1,13 @@
+package dutchChocolates.panMan.appLayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLayerApplication.class, args);
+	}
+
+}
---
+++ appLayer/src/test/java/dutchChocolates/panMan/appLayer/AppLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package dutchChocolates.panMan.appLayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
