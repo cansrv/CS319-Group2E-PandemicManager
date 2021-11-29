8644227c033281303fc19a14c0d52cb3998f7f58
berkturkcapar <63585832+berkturkcapar@users.noreply.github.com>
2021-11-29 20:52:08 +0300

    Added Login Page Details

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -12,11 +12,19 @@ const Login = () => {
     return (
         <div className = "mainContainer">
             <div className = "row">
-                <div className = "col-6 leftSide">
-                    Left Side
+                <div className = "col-6 justify-content-center text-center leftContainer">
+                    <div className = "row col-12 justify-content-center text-center logoContainer">
+                        Logo
+                    </div>
+                    <div className = "row col-12 justify-content-center text-center nameContainer">
+                        PANMAN
+                    </div>
+                    <div className = "row col-12 justify-content-center text-center aboutContainer">
+                        “blandit turpis cursus in hac habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris a diam maecenas sed enim ut sem”
+                    </div>
                 </div>
                 {console.log("I am at the login page")}
-                <div className = "col-6 rightSide">
+                <div className = "col-6 rightContainer">
                     Right Side
                 </div>
             </div>
