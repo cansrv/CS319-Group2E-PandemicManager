c395999881039660531d6449e17b449b27380868
YigitEkin <yigit.ekin@ug.bilkent.edu.tr>
2021-11-29 20:53:02 +0300

    start signup

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,27 +1,7 @@
 import "../css/Login.css"
 
 const Login = () => {
-    const goToSignUp = () => {
-        
-    };
 
-    const login = () => {
-        
-    };
-
-    return (
-        <div className = "mainContainer">
-            <div className = "row">
-                <div className = "col-6 leftSide">
-                    Left Side
-                </div>
-                {console.log("I am at the login page")}
-                <div className = "col-6 rightSide">
-                    Right Side
-                </div>
-            </div>
-        </div>
-    )
 }
 
 export default Login
