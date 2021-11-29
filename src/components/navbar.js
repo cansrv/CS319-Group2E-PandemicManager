9e049868b5e77d2452d10ba409a9f88375dfb1c9
canonal7 <55083192+canonal7@users.noreply.github.com>
2021-11-29 21:27:24 +0300

    navbar
    navbar init

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+
+import '../css/navbar.css'
+
+const Navbar = () => {
+
+	return(
+		<div className = 'navbar'>
+			<a href = "/" > <img src = ></img></a>
+		</div>
+	)
+}
+
+export default Navbar
