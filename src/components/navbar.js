48696ff7c63b6e9a16ad810416825805dab08ee3
canonal7 <55083192+canonal7@users.noreply.github.com>
2021-11-30 14:36:40 +0300

    navbar changed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,8 +4,20 @@ import '../css/navbar.css'
 const Navbar = () => {
 
 	return(
-		<div className = 'navbar'>
-			<a href = "/" > <img src = ></img></a>
+		<div className = 'row navbar'>
+			<div className= 'col-2 logoContainer'>
+				<a className = "logoContainer" href="/">v</a>
+			</div>
+			<div className="col-7 brandContainer">
+				<p id="brandName">PanMan</p>
+				<p id="brandDesc">Bilkent University Pandemic Manager</p>
+				 
+			</div>
+			<div className = "col-3  userInfo">
+				<p id="nameSurname">Name Surname</p>
+				<p id="userType">User Type</p>
+			</div>
+			
 		</div>
 	)
 }
