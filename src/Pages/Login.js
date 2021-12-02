84d75e97867602ea3547a775b15c287f1ec55747
berkturkcapar <63585832+berkturkcapar@users.noreply.github.com>
2021-12-02 18:29:14 +0300

    login responsive design

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -4,42 +4,42 @@ import { Link } from "react-router-dom"
 const Login = () => {
     return (
         <div className = "mainContainer">
-            <div className="seperator"></div>
+            <div className="seperator d-none d-"></div>
             <div className = "row">
-                <div className = "col-6 justify-content-center text-center leftContainer">
+                <div className = "col-12 col-md-6 justify-content-center text-center leftContainer">
                     <div className = "row col-12 justify-content-center text-center logoContainer"></div>
                     <div className = "row col-12 justify-content-center text-center nameContainer">
                         PANMAN
                     </div>
-                    <div className = "row col-12 justify-content-center text-center aboutContainer">
+                    <div className = "row d-none d-md-block col-md-12 justify-content-center text-center aboutContainer">
                         Welcome stranger! PanMan is a pandemic manager application for all Bilkenters. 
                         But, it is not only an app, it is an idea, it is a way of life. 
                         You are free and recommended to use it, but come in at your own risk…
                     </div>
                 </div>
-                <div className = "col-6 rightContainer">
-                    <form className>
-                        <div class="form-group row">
+                <div className = "d-flex col-12 col-md-6 align-items-center rightContainer justify-content-between">
+                    <form className = "col-12">
+                        <div class="form-group flex-row d-flex justify-content-between align-items-center">
                             <label for="inputEmail3" class="col-sm-2 col-form-label label">Email: </label>
                             <div class="col-8 inputGroup">
-                                <input type="email" class="form-control emailInput" id="inputEmail3" placeholder="Email"/>
+                                <input type="email" class="form-control" id="inputEmail3" placeholder="Email"/>
                             </div>
                         </div>
-                        <div class="form-group row ">
+                        <div class="form-group flex-row d-flex justify-content-between align-items-center">
                             <label for="inputPassword3" class="col-sm-2 col-form-label label">Password: </label>
                             <div class="col-8 inputGroup">
-                                <input type="password" class="form-control passwordInput" id="inputPassword3" placeholder="Password"/>
+                                <input type="password" class="form-control" id="inputPassword3" placeholder="Password"/>
                             </div>
                         </div>
-                        <div class="form-group row">
-                            <div class="col-sm-10 buttonContainer">
+                        <div class="form-group flex-row d-flex justify-content-center align-items-center">
+                            <div class="buttonContainer">
                                 <button type="submit" href= "#" class="btn loginButton">Log-in</button>
                             </div>
                         </div>
+                        <div className = "signupPrompt flex-row d-flex justify-content-center align-items-center">
+                            Don’t have an Account? <Link to = "/signup" className="pl-2" >Sign-up</Link>
+                        </div>
                     </form>
-                    <div className = "signupPrompt">
-                        Don’t have an Account? <Link to = "/signup" ><p className="signupLink">Sign-up</p></Link>
-                    </div>
                 </div>
             </div>
         </div>
