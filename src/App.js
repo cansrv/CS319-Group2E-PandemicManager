89e91eb7d1637130358bc7a202f48f70aec3461b
YigitEkin <yigit.ekin@ug.bilkent.edu.tr>
2021-12-02 18:25:41 +0300

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@
 import { BrowserRouter, Router, Route, Routes, Switch } from 'react-router-dom';
 import Login from './Pages/Login';
 import {SignUp} from './Pages/SignUp';
+import Navbar from './Components/Navbar'
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path = "/" element={<Login></Login>}>
+        <Route path = "/login" element={<Login></Login>}>
         </Route>
+          <Route path="/" element={<Navbar/>}></Route>
         <Route path = "/signup" element={<SignUp></SignUp>}>
         </Route>
       </Routes>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,35 @@
 
-import '../css/navbar.css'
+import '../css/Navbar.css'
 
 const Navbar = () => {
 
 	return(
-		<div className = 'row navbar'>
-			<div className= 'col-2 logoContainer'>
-				<a className = "logoContainer" href="/">v</a>
+		<nav className="navbar navbar-expand-lg fixed-top panman-bg container d-flex">
+			<button className="navbar-toggler d-none" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+					aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon">fghbjbjjjbjbjb</span>
+			</button>
+			{/*LİNK KOY*/}
+			<div className="row navbar-content-area">
+				<div className="col-lg-7 col-md-12 d-flex offset-1 offset-md-0
+				justify-content-md-center justify-content-center align-items-center">
+					<a className="navbar-brand" href="#"><div className="panman-logo"></div></a>
+					<span className="navbar-logo-text ml-md-4 ml-1">PanMan</span>
+					<span className="navbar-name-text ml-lg-4 ml-3 d-none d-sm-inline">University Pandemic Manager</span>
+				</div>
+				<div className="col-lg-5 d-flex align-items-center">
+					<div className="ml-auto d-flex align-items-center">
+						<div className="d-none d-lg-block user-logo mr-3"></div>
+						<div className="d-none d-lg-block">
+							<div>
+								<div className="username">Mehmet Yılmaz</div>
+								<div className="account-status text-muted">Student</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div className="col-7 brandContainer">
-				<p id="brandName">PanMan</p>
-				<p id="brandDesc">Bilkent University Pandemic Manager</p>
-				 
-			</div>
-			<div className = "col-3  userInfo">
-				<p id="nameSurname">Name Surname</p>
-				<p id="userType">User Type</p>
-			</div>
-			
-		</div>
+		</nav>
 	)
 }
 
