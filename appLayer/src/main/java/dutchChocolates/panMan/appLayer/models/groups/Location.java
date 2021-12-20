2d475df17565ceabd4a74a5cdc921ff982fea479
ÖvgümSezen <ovgumcansezen@gmail.com>
2021-12-20 18:19:46 +0300

    Create Location.java

---
+++ appLayer/src/main/java/dutchChocolates/panMan/appLayer/models/groups/Location.java
@@ -0,0 +1,24 @@
+package dutchChocolates.panMan.appLayer.models.groups;
+
+public class Location {
+    //Properties
+    private String name;
+
+    //Constructors
+    public Location(){}
+
+    public Location(String name){
+        this.name = name;
+    }
+
+    //Methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
