b13747867dbfaaf645924bfe499d25396539df87
haruncansurav <can.surav@ug.bilkent.edu.tr>
2021-12-21 14:57:36 +0300

    Mediator signatures finished

--- appLayer/src/main/java/dutchChocolates/panMan/appLayer/models/mediators/SectionMediator.java
+++ appLayer/src/main/java/dutchChocolates/panMan/appLayer/models/mediators/SectionMediator.java
@@ -1,4 +1,42 @@
 package dutchChocolates.panMan.appLayer.models.mediators;
 
+import dutchChocolates.panMan.appLayer.models.actors.Student;
+import dutchChocolates.panMan.appLayer.models.classes.Course;
+import dutchChocolates.panMan.appLayer.models.classes.Lecture;
+import dutchChocolates.panMan.appLayer.models.classes.Section;
+
+import java.util.List;
+
 public class SectionMediator {
+
+    private static SectionMediator sectionMediator = null;
+
+
+    //Constructors
+    private SectionMediator(){}
+
+
+    //Methods
+    public SectionMediator getInstance(){
+        if (sectionMediator == null) {
+            sectionMediator = new SectionMediator();
+        }
+        return sectionMediator;
+    }
+
+    public List<Student> getRiskyStudents(Section section) {
+        return null;
+    }
+
+    public boolean setOnline(Section section, boolean isOnline) {
+        return true;
+    }
+
+    public boolean cancelClass(Section section) {
+        return true;
+    }
+
+    public boolean addLecture(Section section, Lecture lecture) {
+        return true;
+    }
 }
--- appLayer/src/main/java/dutchChocolates/panMan/appLayer/models/mediators/StaffCreatedGroupMediator.java
+++ appLayer/src/main/java/dutchChocolates/panMan/appLayer/models/mediators/StaffCreatedGroupMediator.java
@@ -1,4 +1,39 @@
 package dutchChocolates.panMan.appLayer.models.mediators;
 
+import dutchChocolates.panMan.appLayer.models.Group;
+import dutchChocolates.panMan.appLayer.models.User;
+
+import java.util.List;
+
 public class StaffCreatedGroupMediator {
+
+
+    private static StaffCreatedGroupMediator staffCreatedGroupMediator = null;
+
+    private StaffCreatedGroupMediator() {
+    }
+
+    public StaffCreatedGroupMediator getInstance() {
+        if (staffCreatedGroupMediator == null) {
+            staffCreatedGroupMediator = new StaffCreatedGroupMediator();
+        }
+        return staffCreatedGroupMediator;
+    }
+
+    public boolean addParticipant(Group group, User user) {
+        return true;
+    }
+
+    public boolean addParticipants(Group group, List<User> users) {
+        return true;
+    }
+
+    public boolean dismissFromGroup(Group group, User user) {
+        return true;
+    }
+
+    public boolean endGroup(Group group) {
+        return true;
+    }
+
 }
