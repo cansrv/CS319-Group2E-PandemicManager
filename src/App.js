cd3e129c9f77a1be9330d53792e925050748be45
canonal7 <55083192+canonal7@users.noreply.github.com>
2021-12-22 15:24:28 +0300

    App.js updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Login from './Pages/Login';
 import { SignUp } from './Pages/SignUp';
 import StaffCampusPage from './Pages/StaffCampusPage';
 import StudentHomePage from "./Pages/StudentHomePage"
-import InstructorCoursePage from './Pages/InstructorCoursePage';
+import StudentCoursesPage from './Pages/StudentCoursesPage';
 import InstructorCampusPage from './Pages/InstructorCampusPage';
 import Navbar from './components/navbar'
 import StudentTestsPage from "../src/Pages/StudentTestsPage"
@@ -13,8 +13,9 @@ function App() {
     return ( 
         <BrowserRouter>
             <Routes>
-                <Route path = "/login" element = { <Login></Login>}></Route> 
-                <Route path = "/studentHomePage"exact element = { <StudentHomePage/>}></Route> 
+                <Route path = "/" element = { <Login></Login>}></Route> 
+                <Route path = "/studentHomePage"exact element = { <StudentHomePage/>}></Route>
+                <Route path= "/studentCoursesPage"  element={<StudentCoursesPage />}></Route>
                 <Route path = "/signup" element = { <SignUp > </SignUp>}></Route>
             </Routes> 
         </BrowserRouter>
