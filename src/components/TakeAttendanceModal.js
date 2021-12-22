90d01f3a445b14b6d49336ea93624af6639f2528
canonal7 <55083192+canonal7@users.noreply.github.com>
2021-12-22 21:28:34 +0300

    ViewDetailedInfo Modal added

--- src/components/TakeAttendanceModal.js
+++ src/components/TakeAttendanceModal.js
@@ -1,16 +1,11 @@
-const riskyStudents = [
-	{
-		name: "Cenk Duramayan",
-		id: "21903214"
-	},
-]
+
 const TakeAttendanceModal = () =>{
 	return(
 		< div className="modal fade" id = "takeAttendanceModal" tabindex = "-1" role = "dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" >
 			<div className="modal-dialog modal-dialog-centered" role="document">
 				<div className="modal-content">
 					<div className="modal-header">
-						<h5 className="modal-title" id="exampleModalCenterTitle">Take Attendance Modal</h5>
+						<h5 className="modal-title " id="exampleModalCenterTitle"></h5>
 						<button type="button" className="close" data-dismiss="modal" aria-label="Close">
 							<span aria-hidden="true">&times;</span>
 						</button>
