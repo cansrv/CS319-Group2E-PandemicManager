b6b6ed5f35707241bb79424737bc5f753f4572d6
YigitEkin <yigit.ekin@ug.bilkent.edu.tr>
2021-12-25 14:44:25 +0300

    Connectted student courses page to react

--- src/redux/redux_state.js
+++ src/redux/redux_state.js
@@ -5,8 +5,58 @@ export const state_prototype = {
     accountType: "student",
     HEScode: "22EF56",
     groups: [],
-    attendance: [],
-    courses: [],
+    attendance: [
+        {
+            week: 1,
+            attendance: '3/3'
+        },
+        {
+            week: 2,
+            attendance: '2/3'
+        },
+        {
+            week: 3,
+            attendance: '0/3'
+        },
+        {
+            week: 4,
+            attendance: '1/3'
+        },
+        {
+            week: 5,
+            attendance: '3/3'
+        },
+
+    ],
+    courses: [
+        {
+            name: 'CS-319',
+            section: 1,
+            instructor: 'Eray Tuzun',
+            type: 'Face to Face',
+        },
+        {
+            name: 'CS-315',
+            section: 4,
+            instructor: 'Eray Tuzun',
+            type: 'Face to Face',
+
+        },
+        {
+            name: 'CS-319',
+            section: 1,
+            instructor: 'Eray Tuzun',
+            type: 'Face to Face',
+
+        },
+        {
+            name: 'CS-319',
+            section: 1,
+            instructor: 'Eray Tuzun',
+            type: 'Face to Face',
+
+        }
+    ],
     email: "ter_no@gmail.com",
     isCovid: false,
     tests: [],
